b06c5941affbab05f552d1bceb3ce431a07ef8a8
soyeon <dev.vsoyeon@gmail.com>
2024-04-21 01:13:03 +0900

    LoginPage, JoinPage 구조 작업

--- src/pages/join/JoinPage.js
+++ src/pages/join/JoinPage.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import Layout from "../../layouts/Layout";
+import { InfoBox, InnerWrap, JoinButton, Wrap } from "../../styles/join/JoinStyle";
 
 const JoinPage = () => {
   return (
-    <div>slkdfjsdkjkfjsdklfjsdjfsjdfsl</div>
-  )
-}
+    <>
+      <Layout />
+      <Wrap>
+        <InnerWrap>
+          <div className="join-title">회원가입</div>
+          <InfoBox>
+            <div className="bundle">
+              <p>아이디</p>
+              <input className="input1"/>
+              <button>중복확인</button>
+            </div>
+            <div className="bundle">
+              <p>비밀번호</p>
+              <input className="input2"/>
+            </div>
+            <div className="bundle">
+              <p>비밀번호 확인</p>
+              <input className="input3"/>
+            </div>
+            <div className="bundle">
+              <p>이름</p>
+              <input className="input4"/>
+            </div>
+            <div className="bundle">
+              <p>닉네임</p>
+              <input className="input5"/>
+              <button>중복확인</button>
+            </div>
+            <div className="bundle">
+              <p>연락처</p>
+              <input className="input6"/>
+              <button>중복확인</button>
+            </div>
+          </InfoBox>
+            <JoinButton>가입하기</JoinButton>
+        </InnerWrap>
+      </Wrap>
+    </>
+  );
+};
 
-export default JoinPage
+export default JoinPage;
