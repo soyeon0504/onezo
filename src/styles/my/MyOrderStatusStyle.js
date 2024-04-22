b3ead6f40206a5ebc3ece4a5bc220808d0d0c020
eunjin40 <dev.eunjin40@gmail.com>
2024-04-22 14:51:18 +0900

    feat: MyOrderStatusPage 퍼블리싱 진행중

--- src/styles/my/MyOrderStatusStyle.js
+++ src/styles/my/MyOrderStatusStyle.js
@@ -1,2 +1,38 @@
 import styled from "@emotion/styled";
 
+export const OrderStatusStyle = styled.div`
+  width: 1000px;
+  /* height: 500px; */
+  border: 1px solid #d9d9d9;
+  padding: 10px 30px;
+  h1 {
+    color: #000;
+    font-family: "Noto Sans";
+    font-size: 20px;
+    font-weight: 700;
+  }
+`;
+export const OrderStatusGraph = styled.div`
+  padding-top: 10px;
+  h2 {
+    color: #000;
+    font-family: "Noto Sans";
+    font-size: 18px;
+    font-weight: 700;
+    margin-bottom: 15px;
+  }
+  div{
+    /* margin: 0 auto;
+    text-align: center; */
+  }
+  span {
+    margin-left: 100px;
+    color: #000;
+    font-family: "Noto Sans";
+    font-size: 14px;
+    font-weight: 700;
+  }
+  span:nth-of-type(1){
+    margin-left: 0;
+  }
+`;
