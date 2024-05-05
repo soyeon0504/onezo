b562e47c9274bcd429daa2de769204b21ec39b98
eunjin40 <dev.eunjin40@gmail.com>
2024-05-05 16:25:34 +0900

    feat : CartPage 연동중

--- src/api/cart/cart_api.js
+++ src/api/cart/cart_api.js
@@ -2,11 +2,23 @@ import axios from "axios";
 import { SERVER_URL } from "../config";
 
 // 장바구니 조회
-export const getCartItem = async (memberid) => {
+export const getCartItem = async ({memberId, setCartListData}) => {
     try {
-        const url = `${SERVER_URL}/api/cart/cartItem/${memberid}`
+        const url = `${SERVER_URL}/api/cart/${memberId}`
 
         const res = await axios.get(url);
+        setCartListData([...res.data]);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// 장바구니 삭제
+export const deleteCartItem = async(cartItemId) => {
+    try {
+        const url = `${SERVER_URL}/api/cart/delete/${cartItemId}`
+
+        const res = await axios.delete(url);
         return res;
     } catch (error) {
         console.log(error)
