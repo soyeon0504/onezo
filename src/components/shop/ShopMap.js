b24c31abf17d0d4d3f7b99b3bd3a8344a529b39b
eunjin40 <dev.eunjin40@gmail.com>
2024-06-06 17:54:49 +0900

    feat : ShopDetailPage 퍼블리싱

---
+++ src/components/shop/ShopMap.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+
+const ShopMap = ({ x, y }) => {
+  const [position, setPosition] = useState({ lat: y, lng: x });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          setPosition({ lat: coords.latitude, lng: coords.longitude });
+        },
+        error => {
+          console.error(error);
+        },
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  const containerStyle = {
+    width: "100%",
+    position: "relative",
+    marginTop: "65px",
+    marginBottom: "100px",
+  };
+
+  const mapStyle = {
+    width: "100%",
+    height: "400px",
+    position: "relative",
+    border: "1px solid #2c39b5",
+    borderRadius: "10px",
+  };
+
+  return (
+    // <div style={containerStyle}>
+    //   <Map
+    //     center={position}
+    //     level={3}
+    //     style={mapStyle}
+    //     apiKey="e68e858bc6425ba8391c100f4203dc1e"
+    //   >
+    //     <MapMarker position={position} />
+    //   </Map>
+    // </div>
+    <div style={{ margin: "50px 0px", border: "1px solid #572A01" }}>
+      <Map // 지도를 표시할 Container
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        style={{
+          // 지도의 크기
+          width: "100%",
+          height: "450px",
+        }}
+        level={4} // 지도의 확대 레벨
+      >
+        <MapMarker // 마커를 생성합니다
+          position={{
+            // 마커가 표시될 위치입니다
+            lat: 33.450701,
+            lng: 126.570667,
+          }}
+        />
+      </Map>
+    </div>
+  );
+};
+
+export default ShopMap;
--- src/pages/shop/ShopDetailPage.js
+++ src/pages/shop/ShopDetailPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Layout from "../../layouts/Layout";
 import {
+  ReviewItem,
   ShopDetailHeader,
   ShopDetailStyle,
+  ShopReviewZone,
 } from "../../styles/shop/ShopDetailStyle";
+import ShopMap from "../../components/shop/ShopMap";
 
 // 더미데이터
 const storeSampleData = {
@@ -14,6 +17,30 @@ const storeSampleData = {
   storePhone: "053-952-0778",
   storeHours: "24시간",
 };
+const reviewData = [
+  {
+    review_id: 1,
+    user_nick: "김그린",
+    user_id: "green",
+    comment: "짱짱 맛있어요",
+    date: "2024.06.07",
+  },
+  {
+    review_id: 2,
+    user_nick: "그리니",
+    user_id: "greeny",
+    comment: "벌써 10번째 시켜먹어요",
+    date: "2024.06.05",
+  },
+  {
+    review_id: 3,
+    user_nick: "김원조",
+    user_id: "onezo",
+    comment:
+      "리뷰가 많은 이유가 있네요! 여러 지점에서 먹어봤는데 경북대점이 제일 맛있게 튀기네요.",
+    date: "2024.06.03",
+  },
+];
 
 const ShopDetailPage = () => {
   return (
@@ -27,12 +54,37 @@ const ShopDetailPage = () => {
             <h2>전화번호 : {storeSampleData.storePhone}</h2>
           </div>
           <div>
-            <button>버튼</button>
-            <button>매장</button>
+            <button>
+              <img src="images/shop/icon_takeout.svg" />
+              <br />
+              포장
+            </button>
+            <button>
+              <img src="images/shop/icon_dinein.svg" />
+              <br />
+              매장
+            </button>
           </div>
         </ShopDetailHeader>
+        <ShopMap x="35" y="127" />
+        <ShopReviewZone>
+          <h1>매장 리뷰</h1>
+          {reviewData.map(item => {
+            return (
+              <ReviewItem key={item.review_id}>
+                <div>
+                  <h2>
+                    {item.user_nick} ({item.user_id})
+                  </h2>
+                  <p>{item.date}</p>
+                </div>
+
+                <h3>{item.comment}</h3>
+              </ReviewItem>
+            );
+          })}
+        </ShopReviewZone>
       </ShopDetailStyle>
-      ShopDetailPage
     </Layout>
   );
 };
